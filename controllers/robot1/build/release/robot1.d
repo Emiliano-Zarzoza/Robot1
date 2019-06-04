3682365a3350fc42769da0a9f0db4aecf80e4d05
Emiliano Zarzoza Estrada <emilianozarzozaestrada@Emilianos-MacBook-Pro.local>
2019-06-04 15:35:26 -0500

    The robot is complete with all its parts and is working as wished, now C code will be changed

---
+++ controllers/robot1/build/release/robot1.d
@@ -0,0 +1,5 @@
+build/release/robot1.o: robot1.c \
+  /Applications/Webots.app/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/include/controller/c/webots/types.h \
+  /Applications/Webots.app/include/controller/c/webots/nodes.h \
+  /Applications/Webots.app/include/controller/c/webots/motor.h
--- controllers/robot1/robot1.c
+++ controllers/robot1/robot1.c
@@ -34,7 +34,11 @@ int main(int argc, char **argv)
    *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
    *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
    */
-   WbDeviceTag my_motor = wb_robot_get_device("Motor");
+   WbDeviceTag wheel_right = wb_robot_get_device("motor_right");
+   WbDeviceTag wheel_left = wb_robot_get_device("motor_left");
+
+   wb_motor_set_position (wheel_right, INFINITY);
+   wb_motor_set_position (wheel_left, INFINITY);
 
   /* main loop
    * Perform simulation steps of TIME_STEP milliseconds
@@ -54,7 +58,8 @@ int main(int argc, char **argv)
      * Enter here functions to send actuator commands, like:
      * wb_differential_wheels_set_speed(100.0,100.0);
      */
-     wb_motor_set_velocity(my_motor, 50);
+     wb_motor_set_velocity(wheel_right, -5);
+     wb_motor_set_velocity(wheel_left, -5);
   };
 
   /* Enter your cleanup code here */
