6598911c8fdbaf1bf695f2c48d9109b14788fd20
Emiliano Zarzoza Estrada <emilianozarzozaestrada@Emilianos-MacBook-Pro.local>
2019-06-13 14:00:44 -0500

    The torque is not working as hoped, the velocity of 500 rpm and 100 rpm has been set but it gets uncontrolled

--- controllers/robot1/build/release/robot1.d
+++ controllers/robot1/build/release/robot1.d
@@ -2,4 +2,6 @@ build/release/robot1.o: robot1.c \
   /Applications/Webots.app/include/controller/c/webots/robot.h \
   /Applications/Webots.app/include/controller/c/webots/types.h \
   /Applications/Webots.app/include/controller/c/webots/nodes.h \
-  /Applications/Webots.app/include/controller/c/webots/motor.h
+  /Applications/Webots.app/include/controller/c/webots/motor.h \
+  /Applications/Webots.app/include/controller/c/webots/keyboard.h \
+  /Applications/Webots.app/include/controller/c/webots/distance_sensor.h
--- controllers/robot1/robot1.c
+++ controllers/robot1/robot1.c
@@ -12,6 +12,33 @@
  */
 #include <webots/robot.h>
 #include <webots/motor.h>
+#include <webots/keyboard.h>
+#include <webots/distance_sensor.h>
+
+#include <stdio.h>
+#include <math.h>
+/*
+enum {
+ WB_KEYBOARD_END,
+ WB_KEYBOARD_HOME,
+ WB_KEYBOARD_LEFT,
+ WB_KEYBOARD_UP,
+ WB_KEYBOARD_RIGHT,
+ WB_KEYBOARD_DOWN,
+ WB_KEYBOARD_PAGEUP,
+ WB_KEYBOARD_PAGEDOWN,
+ WB_KEYBOARD_NUMPAD_HOME,
+ WB_KEYBOARD_WB_KEYBOARD_PAGEDOWN,
+ WB_KEYBOARD_NUMPAD_UP,
+ WB_KEYBOARD_NUMPAD_RIGHT,
+ WB_KEYBOARD_NUMPAD_DOWN,
+ WB_KEYBOARD_NUMPAD_END,
+ WB_KEYBOARD_KEY,
+ WB_KEYBOARD_SHIFT,
+ WB_KEYBOARD_CONTROL,
+ WB_KEYBOARD_ALT
+};
+ */
 
 /*
  * You may want to add macros here.
@@ -27,6 +54,15 @@ int main(int argc, char **argv)
 {
   /* necessary to initialize webots stuff */
   wb_robot_init();
+  wb_keyboard_enable(TIME_STEP); //sample period for the keyboard, the same as
+                                // TIME_STEP, to use our real keyboard on wb
+  int pressed_key;
+  int MinRange = 2;
+  float m_step;
+  int MaxRange = 10000;
+  int bits = 1024;
+  float dist_cm;
+
 
   /*
    * You should declare here WbDeviceTag variables for storing
@@ -34,11 +70,21 @@ int main(int argc, char **argv)
    *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
    *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
    */
+   // Motor devices
    WbDeviceTag wheel_right = wb_robot_get_device("motor_right");
    WbDeviceTag wheel_left = wb_robot_get_device("motor_left");
 
    wb_motor_set_position (wheel_right, INFINITY);
    wb_motor_set_position (wheel_left, INFINITY);
+
+   // Distance sensor devices
+   WbDeviceTag dist_sensor  = wb_robot_get_device("distance_sensor");
+
+   wb_distance_sensor_enable (dist_sensor, TIME_STEP);
+
+   double ds_value;
+
+   //double ds_value;
 
   /* main loop
    * Perform simulation steps of TIME_STEP milliseconds
@@ -51,6 +97,15 @@ int main(int argc, char **argv)
      * Enter here functions to read sensor data, like:
      *  double val = wb_distance_sensor_get_value(my_sensor);
      */
+     pressed_key= wb_keyboard_get_key();
+
+
+     ds_value = wb_distance_sensor_get_value (dist_sensor);
+     m_step = (MaxRange - MinRange) / (bits - 1);
+     dist_cm = MinRange + (ds_value * m_step);
+
+     printf ("Distance sensor: %lf\n", ds_value);
+
 
     /* Process sensor data here */
 
@@ -58,8 +113,42 @@ int main(int argc, char **argv)
      * Enter here functions to send actuator commands, like:
      * wb_differential_wheels_set_speed(100.0,100.0);
      */
-     wb_motor_set_velocity(wheel_right, -5);
-     wb_motor_set_velocity(wheel_left, -5);
+
+     if (pressed_key == WB_KEYBOARD_UP)
+     {
+       wb_motor_set_velocity (wheel_right, -52.41);
+       wb_motor_set_velocity (wheel_left, -52.41);
+       printf ("UP key pressed!\n");
+
+     }
+
+     else if (pressed_key == WB_KEYBOARD_DOWN)
+     {
+       wb_motor_set_velocity (wheel_right, 10.4720);
+       wb_motor_set_velocity (wheel_left, 10.4720);
+       printf ("DOWN key pressed!\n");
+     }
+
+     else if (pressed_key == WB_KEYBOARD_RIGHT)
+     {
+       wb_motor_set_velocity(wheel_right, 6.28);
+       wb_motor_set_velocity(wheel_left, -6.28);
+       printf ("RIGHT key pressed!\n");
+     }
+
+     else if (pressed_key == WB_KEYBOARD_LEFT)
+     {
+       wb_motor_set_velocity(wheel_right, -6.28);
+       wb_motor_set_velocity(wheel_left, 6.28);
+       printf ("LEFT key pressed!\n");
+     }
+
+     else
+     {
+       wb_motor_set_velocity (wheel_right, 0);
+       wb_motor_set_velocity (wheel_left, 0);
+     }
+
   };
 
   /* Enter your cleanup code here */
